a8e0272d8b0051e1b4a81cc2d8e8ff8ebf060133
Patrick <patrickveralopez@gmail.com>
2021-04-29 14:18:14 -0400

    Add files

---
+++ InfovidWeb/Controllers/ComunaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using InfovidWeb.Models;
+using System.Web.Mvc;
+
+namespace InfovidWeb.Controllers
+{
+    public class ComunaController : Controller
+    {
+        // GET: Comuna
+        public ActionResult Index()
+        {
+            List<ComunaCLS> listaComuna = null;
+            using (var bd=new BDComuna())
+            {
+                listaComuna = (from comuna in bd.Comuna
+                              select new ComunaCLS
+                              {
+                                  idcomuna = comuna.IdComuna,
+                                  nombre = comuna.Nombre,
+                                  fase = comuna.Fase
+                              }).ToList();
+            }
+                return View(listaComuna);
+        }
+    }
+}
---
+++ InfovidWeb/Models/ComunaCLS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace InfovidWeb.Models
+{
+    public class ComunaCLS
+    {
+        [Display(Name = "ID Comuna")]
+        public int idcomuna { get; set; }
+        [Display(Name = "Nombre Comuna")]
+        public string nombre { get; set; }
+        [Display(Name = "Fase Comuna")]
+        public int fase { get; set; }
+    }
+}
